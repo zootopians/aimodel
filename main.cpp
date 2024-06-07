94d938cb9595197501b76c31e08827238e4d4c19
seekdude <rohan.nandu358@gmail.com>
2024-06-07 22:59:59 +0545

    huhu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"hello rabbit";
-    cout<<"forgot to say you are cute";
-    cout<<"well taking this as a edit";
+    cout<<"hello rabbit"<<endl;
+    cout<<"forgot to say you are cute"<<endl;
+    cout<<"well how about we celebrate with a kiss which you can give me on sunday.";
 }
